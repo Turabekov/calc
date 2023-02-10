b5a17a2f576eacdb66caef7ec7acd8b82a603a95
khumoyun <turabekov1110@gmail.com>
2023-02-10 19:24:52 +0500

    print page users

--- cmd/main.go
+++ cmd/main.go
@@ -9,35 +9,25 @@ import (
 
 func main() {
 
-	controller.CreateUser(
-		models.User{
-			Id:      1,
-			Name:    "Shohruh",
-			Surname: "Safarov",
-		},
-	)
+	controller.GenerateUser(100)
 
-	controller.CreateUser(
-		models.User{
-			Id:      2,
-			Name:    "Abduqodir",
-			Surname: "Musayev",
-		},
-	)
+	fmt.Println("Page raqamini kiriting:")
+	pageNumber := 0
+	fmt.Scan(&pageNumber)
 
-	for _, user := range controller.GetListUser() {
+	pageOffset := pageNumber * 10
+
+	users, err := controller.GetListUser(models.GetListRequest{
+		Offset: pageOffset,
+		Limit:  10,
+	})
+
+	if err {
+		fmt.Println("users out of range")
+		return
+	}
+
+	for _, user := range users {
 		fmt.Println(user)
 	}
-	// Get By Id
-	user, err := controller.GetUserById(3)
-	fmt.Println("Get By id:", user, "error:", err)
-	// Update User
-	userUpdated, err2 := controller.UpdateUser(2)
-	fmt.Println("Updated user:", userUpdated, "error:", err2)
-	fmt.Println(controller.GetListUser())
-	// Delete User
-	err3 := controller.DeleteUser(2)
-	fmt.Println("error for deleting:", err3)
-	fmt.Println(controller.GetListUser())
-
 }
--- controller/user.go
+++ controller/user.go
@@ -2,16 +2,14 @@ package controller
 
 import (
 	"app/models"
+
+	"github.com/bxcodec/faker/v3"
 )
 
 var Users []models.User
 
 func CreateUser(data models.User) {
 	Users = append(Users, data)
-}
-
-func GetListUser() []models.User {
-	return Users
 }
 
 // getbyid
@@ -48,4 +46,23 @@ func DeleteUser(id int) bool {
 	}
 
 	return true
+}
+
+func GetListUser(req models.GetListRequest) (resp []models.User, err bool) {
+
+	if req.Limit+req.Offset > len(Users) {
+		return []models.User{}, true
+	}
+
+	return Users[req.Offset : req.Limit+req.Offset], false
+}
+
+func GenerateUser(count int) {
+	for i := 0; i < count; i++ {
+		Users = append(Users, models.User{
+			Id:      i + 1,
+			Name:    faker.FirstName(),
+			Surname: faker.LastName(),
+		})
+	}
 }
